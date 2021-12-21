20eaed18aeda5024de115475927f5580c4d08783
Mohamed Mohamud <momo206@uw.edu>
2021-12-20 19:11:56 -0800

    5 NBA questions

---
+++ NBAQuestions.js
@@ -0,0 +1,28 @@
+
+var questions = [
+    {
+        question: 'Which team has won most NBA conference championships?',
+        answers: ['1. Los Angeles Lakers', '2. Miami Heat', '3. Boston Celtics', '4. Denver Nuggets'],
+        correctAnswer: '1. Los Angeles Lakers'
+    },
+    {
+        question: 'Who holds the record for the most points scored over their career?',
+        answers: ['1. Tim Duncan', '2. Micheal Jordan', '3. Lebron James', '4. Kareem Abdul-Jabbar'],
+        correctAnswer: '4. all of the above'
+    },
+    {
+        question: 'Which player was drafted #1 at the 2003 NBA Draft?',
+        answers: ['1. Chris Bosh', '2. Lebron James', '3. Carmelo Anthony', '4. Chris Paul'],
+        correctAnswer: '2. Lebron James'
+    },
+    {
+        question: 'Which of these franchises never relocated?',
+        answers: ['1. Warriors', '2. Utah Jazz', '3. Toronto Raptors', '4. Los Angeles Clippers'],
+        correctAnswer: '3. Toronto Raptors'
+    },
+    {
+        question: 'Who is the most recent rookie of the year?',
+        answers: ['1. James Wiseman', '2. Anthony Edwards', '3. Cole Anthony', '4. Lamelo Ball'],
+        correctAnswer: '4. Lamelo Ball'
+    }
+];
